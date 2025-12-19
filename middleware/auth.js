528a7b6c8f70c9b0bd3586c34f71906e3f8ef27a
sahil852963 <sahilsharma83999@gmail.com>
2025-12-19 16:35:09 +0530

    added controller

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,14 +1,14 @@
 import jwt from "jsonwebtoken";
 
 export function authMiddleware(req, res, next) {
-  const token = req.headers.authorization?.split(" ")[1];
-  if (!token) return res.status(401).json({ message: "Unauthorized no token" });
+	const token = req.headers.authorization?.split(" ")[1];
+	if (!token) return res.status(401).json({ message: "Unauthorized no token" });
 
-  try {
-    const verified = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = verified;
-    next();
-  } catch {
-    return res.status(401).json({ message: "Token invalid" });
-  }
+	try {
+		const verified = jwt.verify(token, process.env.JWT_SECRET);
+		req.user = verified;
+		next();
+	} catch {
+		return res.status(401).json({ message: "Token invalid" });
+	}
 }
